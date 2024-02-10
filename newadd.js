8474519f4170a197c2da65ac4b00b030e1f21e10
DevSujon <sujonmaia274@gmail.com>
2024-02-10 10:32:55 +0600

    creats elements

---
+++ newadd.js
@@ -0,0 +1,35 @@
+
+
+const addN = document.getElementById('newadd');
+
+const newli = document.createElement('li');
+newli.innerText = "Md Sujon Mia Sujon";
+
+addN.appendChild(newli);
+
+
+const newsection = document.getElementById('sections');
+
+
+const newsec = document.createElement("section");
+
+
+newsec.innerHTML = `
+
+<h3>md sujon mia sujon </h3>
+ <ul>
+   <li> raru</li>
+   <li> saju</li>
+   <li> foju</li>
+   <li> moju</li>
+   <li> fosku</li>
+   <li> nosku</li>
+ </ul>
+
+`;
+
+newsection.appendChild(newsec);
+
+
+
+
